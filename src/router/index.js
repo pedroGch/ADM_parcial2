bc952c832e56b6b1a2652cb2a181f54a37d07fb5
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-06-09 22:02:36 -0300

    nueva ruta para acceder a la vista de form receta

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import CargarRecetaView from '@/views/CargarRecetaView.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +14,7 @@ const routes = [
   {
     path: '/cargarReceta',
     name: 'cargarReceta',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/CargarRecetaView.vue')
+    component: CargarRecetaView
   }
 ]
 
