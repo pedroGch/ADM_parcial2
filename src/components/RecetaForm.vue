12c52e54dc3d19d69504a33cbb311deb52be1745
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-06-12 18:35:34 -0300

    validation on cargarReceta form

--- src/components/RecetaForm.vue
+++ src/components/RecetaForm.vue
@@ -1,201 +1,237 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12" class="text-center pb-3">
-        <h2>Ingresar Receta</h2>
-      </v-col> 
+
+    <v-row class="text-center mt-3">
+    <v-row >
+      <v-card
+        class="mx-auto"
+        id="card-form"
+      >
+
+
+        <v-card-title class="div-h3">
+          <h3>Ingresar Receta</h3>
+        </v-card-title>
+        <v-divider></v-divider>
+
+        <form class="px-4 pb-4">
+          <v-row class="py-4">
+            <v-col cols="6">
+              <v-text-field
+                cols="6"
+                v-model="nombreReceta"
+                :error-messages="nombreRecetaError"
+                :counter="10"
+                label="Nombre de la receta"
+                required
+                @input="$v.nombreReceta.$touch()"
+                @blur="$v.nombreReceta.$touch()"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-select
+                cols="6"
+                v-model="selectCategoria"
+                :items="categorias"
+                :error-messages="selectErrorsCategoria"
+                label="Categoria"
+                required
+                @change="$v.selectCategoria.$touch()"
+                @blur="$v.selectCategoria.$touch()"
+              ></v-select>
+            </v-col>
+          </v-row>
+          <v-row class="py-4">
+            <v-col cols="12" class="div-h3">
+              <h3>Ingredientes</h3>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                cols="6"
+                v-model="nombreIngrediente"
+                :error-messages="nombreIngredienteErrors"
+                :counter="10"
+                label="Nombre del ingrediente"
+                required
+                @input="$v.nombreIngrediente.$touch()"
+                @blur="$v.nombreIngrediente.$touch()"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                cols="6"
+                v-model="cantidadIngrediente"
+                :error-messages="cantidadIngredienteErrors"
+                :counter="10"
+                label="Cantidad"
+                required
+                @input="$v.cantidadIngrediente.$touch()"
+                @blur="$v.cantidadIngrediente.$touch()"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="4">
+              <v-select
+                cols="6"
+                v-model="selectUnidades"
+                :items="unidades"
+                :error-messages="selectUnidadesErrors"
+                label="Categoria"
+                required
+                @change="$v.selectUnidades.$touch()"
+                @blur="$v.selectUnidades.$touch()"
+              ></v-select>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-textarea
+                v-model="preparacion"
+                name="paracion"
+                label="Preparación"
+                hint="Escrbí la preparación"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <h2>Subir archivo</h2>
+            </v-col>
+            <v-col cols="12">
+              <v-file-input
+                show-size
+                truncate-length="15"
+              ></v-file-input>
+            </v-col>
+          </v-row>
+          <v-btn
+            class="mr-4"
+            @click="submit"
+            color="secondary"
+          >
+            Guardar
+          </v-btn>
+          <v-btn @click="clear" color="secondary">
+            Borrar
+          </v-btn>
+        </form>
+      </v-card>
+    </v-row>
     </v-row>
   </v-container>
 </template>
 
 <script>
+  import { validationMixin } from 'vuelidate'
+  import { required, maxLength } from 'vuelidate/lib/validators'
+
   export default {
+    mixins: [validationMixin],
+    validations: {
+      nombreReceta: { required, maxLength: maxLength(10) },
+      nombreIngrediente: { required, maxLength: maxLength(10) },
+      cantidadIngrediente: { required, maxLength: maxLength(10) },
+      selectUnidades: { required },
+      selectCategoria: { required },
+
+    },
+    components: {
+
+    },
+    
     name: 'RecetaForm',
     data(){
       return{
-        sombraError:false,
-          unidades:[
-            {"nombre": "a gusto" , "id": 0},
-            {"nombre": "kg" , "id": 1},
-            {"nombre": "Un" , "id": 2},
-            {"nombre": "gr" , "id": 3},
-            {"nombre": "lt" , "id": 4},
-            {"nombre": "ml" , "id": 5},
-          ],
-          nombreReceta: "",
-          ingredientes: [],
-          nombre_ingrediente: "",
-          cantidad_ingrediente: "",
-          unidad_nombre: "",
-          mostrar:true,
-          mostrarBoton: false,
-          preparacion: "",
-          categoriaSeleccionada: "",
-          errores: {
-            "nombre": "",
-            "ingredientes": "",
-            "categoria": "",
-            "preparacion": "",
-          },
-          err_nombre:"",
-          err_ingredientes: "",
-          err_categoria: "",
-          err_preparacion: "",
-          imagen_receta: null
+        submitStatus: null,
+        selectCategoria: null,
+        selectUnidades: null,
+        nombreReceta: "",
+        ingredientes: [],
+        nombreIngrediente: "",
+        cantidadIngrediente: "",
+        unidad_nombre: "",
+        mostrar:true,
+        mostrarBoton: false,
+        preparacion: "",
+        categorias: [
+          'Salados',
+          'Dulces',
+          'Fit',
+          'Free Gluten',
+        ],
+
+        unidades:[
+          "a gusto",
+          "kg",
+          "Un",
+          "gr",
+          "lt",
+          "ml"
+        ],
       }
     },
     computed: {
-      existenErrores: function(){
-        return this.errores.length
-      }  
+      selectErrorsCategoria () {
+        const errors = []
+        if (!this.$v.selectCategoria.$dirty) return errors
+        !this.$v.selectCategoria.required && errors.push('La categoría es requerida')
+        return errors
+      },
+      selectUnidadesErrors (){
+        const errors = []
+        if (!this.$v.selectUnidades.$dirty) return errors
+        !this.$v.selectUnidades.required && errors.push('La unidad es requerida')
+        return errors
+      },
+      nombreRecetaError() {
+        const errors = []
+        if (!this.$v.nombreReceta.$dirty) return errors
+        !this.$v.nombreReceta.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.nombreReceta.required && errors.push('Se debe ingresar un nombre para la receta.')
+        return errors
+      },
+      nombreIngredienteErrors () {
+        const errors = []
+        if (!this.$v.nombreIngrediente.$dirty) return errors
+        !this.$v.nombreIngrediente.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.nombreIngrediente.required && errors.push('Se debe ingresar un nombre para la receta.')
+        return errors
+      },
+      cantidadIngredienteErrors () {
+        const errors = []
+        if (!this.$v.cantidadIngrediente.$dirty) return errors
+        !this.$v.cantidadIngrediente.maxLength && errors.push('Name must be at most 10 characters long')
+        !this.$v.cantidadIngrediente.required && errors.push('Se debe ingresar un nombre para la receta.')
+        return errors
+      },
+
     },
     methods:{
-      ingresarIngrediente:function () {
-        let ing = {}
-        if (this.nombre_ingrediente != "" && this.cantidad_ingrediente != "" && this.unidad_nombre != ""){
-          ing.nombre = this.nombre_ingrediente
-          ing.cantidad = this.cantidad_ingrediente
-          ing.unidad = this.unidad_nombre
+      submit () {
+        this.$v.$touch()
+        if (this.$v.$invalid) {
+          this.submitStatus = 'ERROR'
+        } else {
+          // do your submit logic here
 
-          this.ingredientes.push(ing)
-
-          this.mostrar = !this.mostrar
-          this.mostrarBoton = !this.mostrarBoton
-
-          this.limpiarCamposIngredientes()
-        }else{
-          this.imprimirFaltaIngrediente()
+            this.submitStatus = 'OK'
+            console.log("todo ok")
+ 
         }
-        
       },
-      mostrarForm:function () {
-        this.mostrar = !this.mostrar
-        this.mostrarBoton = !this.mostrarBoton
+      clear () {
+        this.$v.$reset()
+        this.selectUnidades = ''
+        this.nombreReceta = ''
+        this.nombreIngrediente = ''
+        this.cantidadIngrediente = ''
+        this.selectCategoria = null
+        this.preparacion = ''
       },
-
-      limpiarCamposIngredientes:function(){
-        this.nombre_ingrediente = ""
-        this.cantidad_ingrediente = ""
-        this.unidad_nombre = ""
-        
-      },
-
-      limpiarTodosLosCampos:function (){
-        this.limpiarCamposIngredientes()
-        this.categoriaSeleccionada = ""
-        this.nombreReceta = ""
-        this.preparacion = ""
-        this.ingredientes = []
-
-      },
-
-      eliminarIngrediente:function (indice){
-        console.log(indice)
-        if (indice == 0){
-          this.ingredientes.splice(indice, 1)
-        }else if (indice == this.ingredientes.length){
-          this.ingredientes.pop();
-        }
-        this.ingredientes.splice(indice, indice)
-
-      },
-
-      guardarReceta:function (){
-        this.recetErrores();
-        let receta = {
-          nombre: "",
-          categoria: "",
-          ingredientes: [],
-          preparacion: "",
-          imagen_ruta: null,
-          alt: ""
-          
-        }
-    
-        if (this.validarFormulario()){
-          receta.nombre = this.nombreReceta
-          receta.categoria = this.categoriaSeleccionada
-          receta.ingredientes = this.ingredientes
-          receta.preparacion = this.preparacion
-          receta.alt = "imagen representativa de la receta " + receta.nombre
-          if (this.imagen_receta){
-            receta.imagen_ruta = this.imagen_receta
-          }else{
-            receta.imagen_ruta = "/img/receta-predeterminada.jpg"
-          }
-          this.actualizarLocalStorage(receta)
-          this.mostrarCartelExito()
-        }else{
-          this.mostrarErrores()
-        }
-
-        
-      },
-
-      actualizarLocalStorage:function (unaReceta){
-        let libroDeRecetas = [];
-        let jsonlibroDeRecetas = localStorage.getItem('libroDeRecetas');
-        if (jsonlibroDeRecetas != undefined){
-          libroDeRecetas = JSON.parse(jsonlibroDeRecetas);
-        }
-
-        libroDeRecetas.push(unaReceta);
-        localStorage.setItem('libroDeRecetas', JSON.stringify(libroDeRecetas));
-
-        this.limpiarCamposIngredientes()
-        this.limpiarTodosLosCampos()
-
-      },
-      validarFormulario: function (){
-        let bandera = true
-        if (this.nombreReceta == ""){
-          bandera = false
-          this.errores.nombre = 'El nombre de la receta es obligatorio.'
-        }
-        if(this.ingredientes.length <= 1 ){
-          bandera = false
-          this.errores.ingredientes = 'Debe ingresar más de un elemento.'
-        }
-        if(this.categoriaSeleccionada == ""){
-          bandera = false
-          this.errores.categoria = 'Se debe seleccionar una categoria.'
-        }
-        if(this.preparacion == ""){
-          bandera = false
-          this.errores.preparacion = 'Es necesario que se describa la preparación.'
-        }
-        this.sombraError = !bandera
-        return bandera
-
-      },
-      mostrarErrores: function(){
-        this.err_nombre = this.errores["nombre"]
-        this.err_categoria = this.errores["categoria"]
-        this.err_ingredientes = this.errores["ingredientes"]
-        this.err_preparacion = this.errores["preparacion"]
-      },
-      recetErrores: function (){
-        this.errores.nombre = ""
-        this.errores.ingredientes = ""
-        this.errores.categoria = ""
-        this.errores.preparacion = ""
-        this.err_nombre = ""
-        this.err_categoria = ""
-        this.err_ingredientes = ""
-        this.err_preparacion = ""
-        this.sombraError = false
-      },
-      subirImagen:function (file){
-
-        const fileReader = new FileReader();
-        fileReader.readAsDataURL(file.target.files[0])
-
-        fileReader.onload = (event) => {
-          this.imagen_receta = event.target.result
-          
-        };
-      },
-
     }
   }
 </script>
+<style>
+  #card-form{
+    max-width: 80vw;
+  }
+</style>
