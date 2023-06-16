0c3629f8b9bcc834e320ed72426d8276788a3a32
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-06-16 19:45:48 -0300

    mando por prop la receta para modificar

--- src/views/CargarRecetaView.vue
+++ src/views/CargarRecetaView.vue
@@ -17,7 +17,20 @@
       RecetaForm,
     },
     props:{
-      RecetaParaEditar: null
+      
+    },
+    data(){
+      return{
+        RecetaParaEditar: null,
+      }
+    },
+    mounted: function(){
+      //si me llega por parametro un id para editar lo busco en mi libro de recetas y lo envio como una prop
+      if (this.$route.idEditar != undefined){
+        let misRecetas = JSON.parse(localStorage.getItem('miLibroDeRecetas'))
+        this.RecetaParaEditar = misRecetas[this.$route.idEditar]
+      }
+
     }
   }
 </script>
